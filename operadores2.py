8075d24a2b2498d98e312cdcf7110dbd30e4d6da
barvarajuarez v <barvarajuarez@gmail.com>
2023-07-25 00:38:33 +0000

    operadores

---
+++ operadores2.py
@@ -0,0 +1,12 @@
+base = int(input("Ingresa la base del rectangulo:\t"))
+altura = int(input("Ingresa la altura del rectangulo:\t"))
+
+Area = base * altura
+Perimetro = 2 * (base *  altura)
+
+f"{base}"
+"{}".format(base)
+
+print(f"La base es: {base} [u], la altura es: {altura} [u]")
+print(f"Area: {Area} [u^2]")
+print(f"Perimetro: {Perimetro} [u]")
