4fbc9cc0c0fb908cffc9be8bda1e4d2b8411295c
barvarajuarez v <barvarajuarez@gmail.com>
2023-07-25 00:19:16 +0000

    otros mas

--- e1.py
+++ e1.py
@@ -1,3 +1,39 @@
-print("holis")
+"""print("holis")
 pi=3.1416
 #comentarios de una sola linea
+
+
+
+
+
+
+cadena1 = "hola " + "mundo"
+print(cadena1)
+cadena2 = "buenos días " * 4
+print(cadena2)
+cadena3 = "hola"
+cadena3 += "python"
+print(cadena3)
+
+
+#INDICES
+mensaje = "Universidad Nacional Autonoma de México"
+print(mensaje[1])
+print(mensaje[12:])
+print(mensaje[:12])
+print(mensaje[-6:])
+print(mensaje[12:20])
+print(mensaje[-2:-6])"""
+
+
+mensaje = "El diablo sabe mas por viejo que por diablo"
+print(mensaje.find("diablo"))
+print(mensaje.find("chanclitas"))
+
+
+listadecompras = "jitomate, manzana, pan, yoghurt"
+print("pan" in listadecompras)
+print("lechuga" in listadecompras)
+
+#STRING
+name=(input ("Nombre Completo"))
---
+++ ejercicioCadenas1.py
@@ -0,0 +1,10 @@
+edad = int (input("Escribe tu edad: "))
+nombre = str (input("Escribe tu nombre completo: "))
+estatura = float (input("Escribe tu estatura en metros: "))
+
+print("Tu nombre es: ", nombre)
+print("Tu edad es: ", edad)
+print("Tu estatura es: ", estatura)
+
+print (f"Tu nombre es {nombre}, tienes {edad} años, y mides {estatura} metros ", nombre)
+
